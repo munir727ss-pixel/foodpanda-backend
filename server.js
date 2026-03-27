80d8f647f538a0d3571c57e85efda1b2634612ab
munir727ss-pixel <munir727ss@gmail.com>
2026-03-27 11:50:12 +0500

    Add server code

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Food Backend API is running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
